5d02030776d8f68c5dde3a7670d9b40b3d477ae4
Jim Barnes <jimbarnesdeveloper@gmail.com>
2024-01-25 12:07:01 -0500

    Added css and js that Ramin wrote and integrated it into the gulp process

--- ucf-vertical-slide.php
+++ ucf-vertical-slide.php
@@ -12,15 +12,14 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
+define( 'UCF_VERTICAL_SLIDE__PLUGIN_URL', plugins_url( basename( dirname( __FILE__ ) ) ) );
+define( 'UCF_VERTICAL_SLIDE__STATIC_URL', UCF_VERTICAL_SLIDE__PLUGIN_URL . '/static' );
+define( 'UCF_VERTICAL_SLIDE__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'UCF_VERTICAL_SLIDE__FILEPATH', __FILE__ );
+
 // Shortcode function
-function ucf_vertical_slide_shortcode($atts) {
+function ucf_vertical_slide_shortcode( $atts ) {
     ob_start();
-
-    // Enqueue styles
-    wp_enqueue_style('ucf-vertical-slide-styles', plugin_dir_url(__FILE__) . 'ucf-vertical-slide-styles.css');
-
-    // Enqueue scripts
-    wp_enqueue_script('ucf-vertical-slide-script', plugin_dir_url(__FILE__) . 'ucf-vertical-slide-script.js', array('jquery'), '1.0', true);
 
     ?>
     <div class="container-fluid bg-inverse">
@@ -35,4 +34,18 @@ function ucf_vertical_slide_shortcode($atts) {
 }
 
 // Register the shortcode
-add_shortcode('ucf_vertical_slide', 'ucf_vertical_slide_shortcode');
+add_shortcode( 'ucf_vertical_slide', 'ucf_vertical_slide_shortcode' );
+
+
+function ucf_vertical_slide_enqueue_assets() {
+	$plugin_data = get_plugin_data( UCF_VERTICAL_SLIDE__FILEPATH );
+	$version = $plugin_data['Version'];
+
+	// Enqueue styles
+    wp_enqueue_style( 'ucf-vertical-slide-styles', UCF_VERTICAL_SLIDE__STATIC_URL . '/css/style.min.css', null, $version );
+
+    // Enqueue scripts
+    wp_enqueue_script( 'ucf-vertical-slide-script', UCF_VERTICAL_SLIDE__STATIC_URL . '/js/script.min.js', array( 'jquery' ), $version, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'ucf_vertical_slide_enqueue_assets', 10, 0 );
