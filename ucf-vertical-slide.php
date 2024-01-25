fc6511826e9ef7bcd9e3aac85c56e4dea774acbb
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2024-01-25 10:30:21 -0500

    Readme was modified.

---
+++ ucf-vertical-slide.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: ucf-vertical-slide-plugin
+Description: Custom vetical slide plugin for UCF.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+GitHub Plugin URI: UCF/ucf-vertical-slide-plugin
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
