e38614e8767d9ee0349f1956ed4ab145f34a10cb
Ramin Farhadi <ra680091@CMN9J7HFG4MN.attlocal.net>
2024-01-26 15:04:23 -0500

    ACF fields were created.

--- ucf-vertical-slide.php
+++ ucf-vertical-slide.php
@@ -21,24 +21,40 @@ define( 'UCF_VERTICAL_SLIDE__FILEPATH', __FILE__ );
 include_once 'includes/ucf-vertical-slider-posttype.php';
 
 // Shortcode function
-function ucf_vertical_slide_shortcode( $atts ) {
-    ob_start();
 
-    ?>
-    <div class="container-fluid bg-inverse">
-    <div id="slider-context" class="row overflow sliderParentRow" onmousewheel="scrollFunc(event)">
-        <!-- Content will be dynamically inserted here -->
-    </div>
-    <div id="sliderPagination"></div>
-    </div>
-	<h1>
-	<?php
-	  the_field('silder_title');
-	?>
-	</h1>
-    <?php
+if ( ! function_exists( 'ucf_vertical_slide_shortcode' ) ) {
 
-    return ob_get_clean();
+	function ucf_vertical_slide_shortcode( $atts ) {
+		ob_start();
+		$defaults = array(
+			'slug' => 'default_slug',
+			'id' => 'default_id'
+		);
+
+		// Getting the Id and Slug from shortcode
+		$atts = shortcode_atts($defaults, $atts, 'ucf_vertical_slide');
+		$slider_slug = $atts['slug'];
+		$slider_id = $atts['id'];
+
+		// Getting the data from UCF fields
+		if ( have_rows('vertical_sliders_fields' , $slider_id)){
+
+				$sliders_objects = get_field( 'vertical_sliders_fields' , $slider_id );
+				$my_json_data = json_encode($sliders_objects);
+				echo $my_json_data;
+					}
+		?>
+
+		<div class="container-fluid bg-inverse">
+		<div id="slider-context" class="row overflow sliderParentRow" onmousewheel="scrollFunc(event)">
+			<!-- Content will be dynamically inserted here -->
+		</div>
+		<div id="sliderPagination"></div>
+		</div>
+		<?php
+
+		return ob_get_clean();
+	}
 }
 
 // Register the shortcode
@@ -46,6 +62,7 @@ add_shortcode( 'ucf_vertical_slide', 'ucf_vertical_slide_shortcode' );
 
 
 function ucf_vertical_slide_enqueue_assets() {
+	global $my_json_data; // Access the global variable
 	$plugin_data = get_plugin_data( UCF_VERTICAL_SLIDE__FILEPATH );
 	$version = $plugin_data['Version'];
 
@@ -54,6 +71,7 @@ function ucf_vertical_slide_enqueue_assets() {
 
     // Enqueue scripts
     wp_enqueue_script( 'ucf-vertical-slide-script', UCF_VERTICAL_SLIDE__STATIC_URL . '/js/script.min.js', array( 'jquery' ), $version, true );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'ucf_vertical_slide_enqueue_assets', 10, 0 );
