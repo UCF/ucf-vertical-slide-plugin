0c7152e85a901fdee9705ebce7f70d3006fcc431
Ramin Farhadi <ra680091@CMN9J7HFG4MN.local>
2024-02-15 13:57:50 -0500

    Acf fields defined in codes.

--- ucf-vertical-slide.php
+++ ucf-vertical-slide.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Vertical Slide Plugin
 Description: Custom vertical slide plugin for UCF.
-Version: 0.1.1
+Version: 0.1.2
 Author: UCF Web Communications
 License: GPL3
 GitHub Plugin URI: UCF/ucf-vertical-slide-plugin
